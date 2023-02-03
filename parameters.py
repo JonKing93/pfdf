4f580d162586eeb3c6f94778f0f589ea1c6ab343
Jonathan King <jking@usgs.gov>
2023-02-03 16:17:28 -0700

    remove old docstring from parameters

--- parameters.py
+++ parameters.py
@@ -1,15 +1,8 @@
 """
 parameters  Parameters and configuration details for Step 1
 
-METADATA:
-    name        - The full name of the fire
-    code        - The 3 letter abbreviation
-    state       - The 2 letter state abbreviation
-    location    - The location of the fire
-    start_date  - Start date in MM DD, YYYY format
-"""
 
-# Fire metadata
+"""
 
 
 # Pre-fire assessment
