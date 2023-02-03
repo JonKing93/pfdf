1b7ed53ddb4253a6a05b9505df3c6eb37fb9aa77
Jonathan King <jking@usgs.gov>
2023-02-03 16:00:37 -0700

    Move fire ID into metadata

--- fire.py
+++ fire.py
@@ -7,6 +7,7 @@ VARIABLES:
     state       - The 2 letter state abbreviation
     location    - The location of the fire
     year        - The year the fire started
+    id          - A 7 letter ID for the fire
 """
 
 name = 'Colorado'
@@ -14,3 +15,4 @@ code = 'col'
 state = 'CA'
 location = 'Monterey County'
 year = '2022'
+id = code + year
