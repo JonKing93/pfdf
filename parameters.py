46e661d82711c956d5a1d9d5d479435af506a873
Jonathan King <jking@usgs.gov>
2023-02-03 15:08:48 -0700

    Begin splitting parameters into separate module

---
+++ parameters.py
@@ -0,0 +1,18 @@
+"""
+parameters  Parameters and configuration details for Step 1
+
+METADATA:
+    name        - The full name of the fire
+    code        - The 3 letter abbreviation
+    state       - The 2 letter state abbreviation
+    location    - The location of the fire
+    start_date  - Start date in MM DD, YYYY format
+"""
+
+# Fire metadata
+name = 'Colorado'
+code = 'col'
+state = 'CA'
+location = 'Monterey County, CA'
+start_date = 'January 21, 2022'
+
