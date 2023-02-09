9f9336a598b59f968a158588d741166153a843a0
Jonathan King <jking@usgs.gov>
2023-02-09 09:32:55 -0700

    simplify parameters to algorithm settings

--- parameters.py
+++ parameters.py
@@ -4,21 +4,17 @@ parameters  Parameters and configuration details for Step 1
 
 """
 
-
 # Pre-fire assessment
 pre_fire = False                        # Whether to compute a pre-fire assessment
 evt_version = 140                       # ???
 mtbs_perim_distance_km = 50             # ???
 prefire_percentile_list = [0.5, 0.84]    # The percentiles to compute
 
-# Overwrite permission
-overwrite = False       # Whether Step 1 is allowed to overwrite an existing analysis
-
-# Output options
-make_webtext = True          # Prepare output for the web app
-make_booktest = True         # ??? Perhaps for PDF output
-make_logfile = True          # Whether to create a logfile
-logfile = 'DFModel_Log.txt'  # The name of the logfile
+# Options for designing network basins
+min_basin_size_km2 = 0.025   # Minimum basin size
+max_basin_size_km2 = 8.0     # Maximum basin size
+cell_res = 10.0              # ??? something about resolution. Perhaps 10 meters of topography file?
+burn_acc_threshold = 100     # ??? Original comment is: n_pixels
 
 # Algorithm Options
 segment_guess = 'PERIM'  # ??? Unknown. Options are 'PERIM' and 'NO_PERIM'
@@ -27,12 +23,6 @@ barc_threshold = 'CALC'  # ??? Unknown. Options are 'CALC' and 'DEFINED'
                          # if you have DNBR but no SBS or BARC4, and that the option
                          # has not been tested on recent versions of ArcGIS
                          # (so this option may be defunct)
-
-# Options for designing network basins
-min_basin_size_km2 = 0.025   # Minimum basin size
-max_basin_size_km2 = 8.0     # Maximum basin size
-cell_res = 10.0              # ??? something about resolution. Perhaps 10 meters of topography file?
-burn_acc_threshold = 100     # ??? Original comment is: n_pixels
 
 # Maximum segment length is based on an algorithm option
 if segment_guess == 'PERIM':
@@ -62,21 +52,3 @@ dnbr_unburned = 25
 dnbr_low = 125
 dnbr_mod = 250
 dnbr_high = 500
-
-# Server locations
-server_dir = "P:\\DF_Assessment_GeneralData"
-script_dir = "P:\\Scripts"
-
-# Fire-independent geodatabase file names
-evt_gdb_name = 'LandFire_EVT.gdb'
-landfire_gdb_name = 'LandFire_SAF_SRM.gdb'
-mtbs_gdb_name = 'MTBS_Data.gdb'
-soils_gdb_name = 'STATSGO_Soils.gdb'
-projection_gdb_name = 'ProjectionData.gdb'
-
-# Fire-dependent geodatabase tags
-firein_gdb_tag = 'df_input'
-temp_gdb_tag = 'scratch'
-modelcalcs_gdb_tag = 'dfestimates_utm'
-modelcalcs_web_gdb_tag = 'dfestimates_wgs84'
-
