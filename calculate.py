9da6a559d85d76562ac2eb56479c091726be0db5
Jonathan King <jking@usgs.gov>
2023-02-13 02:25:44 -0700

    docs for calculate.utmzone

--- calculate.py
+++ calculate.py
@@ -1,18 +1,27 @@
 perimeter_ID_field_name = "Perim_ID"
 
 
-def utm(perimeter, centroid, in_centroid, dissolved, utmzone, utmfind, pc_utmzone)
+def utmzone(perimeter, centroid, dissolved, utm, zone):
     """
+    calculate.utmzone  Returns the UTM zone of the centroid of the fire perimeter
+    ----------
+    zone = calculate.utmzone(perimeter, dissolved, centroid, utm, zone)
+    Returns the UTM zone for a fire perimeter. Proceeds by dissolving the fire
+    perimeter into a single polygon and then determining the centroid of that
+    polygon. Projects the centroid into UTM and returns the zone as an int.
+    ----------
     Inputs:
         perimeter (str): The path to the arcpy Feature holding the fire perimeter
         dissolved (str): The path to the arcpy Feature in which to output the
             dissolved / merged perimeter.
         centroid (str): The path to the arcpy Feature in which to output the
             centroid of the fire perimeter
-        in_centroid (str)
-        utmzone (str)
-        utmfind (str)
-        pc_utmzone (str)
+        utm (str): The path to the arcpy feature used to define UTM zones
+        zone (str): The path to the arcpy feature holding the UTM zone of the
+            centroid of the fire perimeter.
+
+    Outputs:
+        zone (int): The UTM zone of the centroid of the fire perimeter
     """
 
     # Reset arcpy environment
@@ -49,12 +58,9 @@ def utm(perimeter, centroid, in_centroid, dissolved, utmzone, utmfind, pc_utmzon
     # Get the centroid of the perimeter
     arcpy.management.FeatureToPoint(dissolved, centroid)
 
-    # Project the centroid into UTM
-    arcpy.management.Project(centroid, utmfind, utmzone)
-
     # Project the centroid into UTM and return the zone
     arcpy.management.Project(centroid, centroid, utm)
-    arcpy.analysis.Identity(centroid, utm, zone)
+    arcpy.analysis.Identify(centroid, utm, zone)
     zone = arcpy.da.TableToNumPyArray(zone, "ZONE")
     zone = zone[0][0]   # ??????? Is this necessary
     return int(zone)
