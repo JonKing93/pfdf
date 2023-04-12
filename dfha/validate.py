a204c4fd4b3c86df02973649a853f4343f3fceb3
jking <jking@usgs.gov>
2023-04-12 11:11:20 -0600

    doc validate.raster

--- dfha/validate.py
+++ dfha/validate.py
@@ -1,9 +1,23 @@
 """
 validate  Functions that validate user inputs
 ----------
-The validate module contains functions that check 
+The validate module contains functions that check user inputs and raise exceptions
+if the inputs are not valid. Some functions allow user inputs to be in multiple
+different formats. When this is the case, the function will usually return the
+input in a standard format for internal processing. For example, the "raster"
+function accepts both file paths and numpy 2D arrays, but will return the
+validated raster as a numpy 2D array. 
 
+The module also defines several custom errors (mainly relating to the shape of
+numpy arrays), which may be used to add identify errors originating from this
+package.
 
+Note on dtypes:
+    In general, it is best to use numpy scalar types to indicate the allowed
+    dtypes of a numpy array. Using built-in types can work, but these typically
+    alias to a specific numpy scalar, rather than a more abstract type. For
+    example, int may alias to numpy.int32, so would not be suitable to allow
+    all integer types. Instead, use numpy.integer to enable all integer types.
 ----------
 Size and type:
     ndarray         - Check that an input is a numpy ndarray
@@ -12,7 +26,7 @@ Size and type:
 
 Numeric numpy:
     non_negative    - Checks that all elements are >= 0
-    integers        - Checks that all elements are integers
+    integers        - Checks that all elements are integers (NOT that the dtype is an int)
 
 GIS:
     raster          - Check that an input is a raster and return it as a numpy 2D array
@@ -22,21 +36,22 @@ Custom Errors:
     ShapeError      - Raised when an input has an incorrect shape
 """
 
-import numpy as np
-from typing import Any, Union, List, Optional, Tuple, Sequence
+from typing import Any, Union, Optional, Tuple, Sequence
 from nptyping import NDArray, Number, Shape
-import numbers
+import numpy as np
 from pathlib import Path
-from dfha.utils import aslist
 import rasterio
+from dfha.utils import aslist, astuple
 
 # Type aliases
-dtype = Union[np.dtype, str, object]
-dtypes = Union[dtype, List[dtype]]
-shape1d = Tuple[int]
+strs = Union[str, Sequence[str]]
+dtypes = Union[type, Sequence[type]]
+shape = Union[int, Sequence[int]]
+shape1d = Union[int, Tuple[int]]
 shape2d = Tuple[int, int]
 raster_array = NDArray[Shape["Rows, Cols"], Number]
 Raster = Union[str, Path, rasterio.DatasetReader, raster_array]
+fill_value = Union[int, float, NDArray[Shape["1"], Number]]
 
 
 def _dtype(input: Any, name: str, allowed: dtypes) -> None:
@@ -46,7 +61,10 @@ def _dtype(input: Any, name: str, allowed: dtypes) -> None:
         actual = input.dtype
         isvalid = [np.issubdtype(actual, type) for type in allowed]
         if not any(isvalid):
-            raise TypeError(f"The dtype of {name} ({actual}) is not allowed.")
+            allowed = ", ".join([str(type)[8:-2] for type in allowed])
+            raise TypeError(
+                f"The dtype of {name} ({actual}) is not allowed. Allowed types are: {allowed}"
+            )
 
 
 def _ndim(input: Any, name: str, N: int) -> None:
@@ -55,10 +73,10 @@ def _ndim(input: Any, name: str, N: int) -> None:
         raise NDimError(name, N, input.ndim)
 
 
-def _shape(input: Any, name: str, axes: List[str], shape: Sequence[int]) -> None:
+def _shape(input: Any, name: str, axes: strs, shape: shape) -> None:
     "Check that the shape of each axis is correct"
     if shape is not None:
-        for axis, required, actual in zip(axes, shape, input.shape):
+        for axis, required, actual in zip(aslist(axes), astuple(shape), input.shape):
             if required != -1 and required != actual:
                 raise ShapeError(name, axis, required, actual)
 
@@ -140,7 +158,7 @@ def ndarray(input: Any, name: str) -> None:
     """
     ndarray  Checks that an input is a numpy ndarray
     ----------
-    ndarray(input)
+    ndarray(input, name)
     Checks that the input is an numpy ndarray. Raises a TypeError if not.
     ----------
     Inputs:
@@ -179,17 +197,45 @@ def non_negative(input: NDArray[Any, Number], name: str) -> None:
 
 
 def raster(
-    raster: Raster,
+    input: Any,
     name: str,
     *,
-    nodata: Optional[numbers.Number] = None,
-    dtypes: Optional[dtypes] = None,
-    shape: Optional[shape2d] = None,
+    nodata: Optional[fill_value] = None,
 ) -> raster_array:
     """
     raster  Check input is valid raster and return as numpy 2D array
     ----------
+    raster(input, name)
+    Checks that the input is a valid raster. Valid rasters may be:
+        * A string with the path to a raster file path,
+        * A pathlib.Path to a raster file,
+        * An open rasterio.DatasetReader object, or
+        * A numpy 2D array with numeric dtype
 
+    Returns the raster as a numpy 2D array. If the input was not a numpy array,
+    then the function will read the raster from file. Rasters will always be
+    read from band 1.
+
+    Raises exceptions if:
+        * a raster file does not exist
+        * a raster file cannot be opened by rasterio
+        * a DatasetReader object is closed
+        * a numpy array does not have 2 dimensions
+        * a numpy array dtype is not a numpy.number
+        * the input is some other type
+
+    raster(..., *, nodata)
+    Specify a value for NoData elements when reading a raster from file. All
+    NoData elements will be replaced with this value. This option has no effect
+    when a numpy array is provided.
+    ----------
+    Inputs:
+        input: The input being checked
+        name: The name of the input for use in error messages
+        nodata: A fill value for NoData elements when reading from file
+
+    Outputs:
+        numpy 2D array, dtype=np.number: The raster represented as a numpy array.
     """
 
     # Convert str to Path
@@ -211,7 +257,7 @@ def raster(
             raster[mask == 0] = nodata
 
     # Require numpy 2D numeric array. Return array
-    matrix(raster, name, dtypes=dtypes, shape=shape)
+    matrix(raster, name, dtypes=np.number)
     return raster
 
 
@@ -255,7 +301,7 @@ def vector(
     ndarray(input, name)
     _ndim(input, name, 1)
     _dtype(input, name, dtypes)
-    _shape(input, name, ["elements"], length)
+    _shape(input, name, "element(s)", length)
 
 
 class NDimError(Exception):
