eaf8ef3794a9249a8e48b1cd1c8cb39654d457a3
jking <jking@usgs.gov>
2023-04-12 10:22:17 -0600

    doc utils

--- dfha/utils.py
+++ dfha/utils.py
@@ -1,26 +1,51 @@
 """
 utils  Utility functions
+----------
+Type conversion:
+    aslist      - Returns an input as a list
+    astuple     - Returns an input as a tuple
+
+Argument Parsing:
+    any_defined - True if any input is not None
 """
 
 from typing import List, Any, Tuple
 
 
 def aslist(input: Any) -> List:
-    "aslist  Place input in list if not already a list."
-    if not isinstance(input, list):
-        input = [input]
+    """
+    aslist  Returns an input as a list
+    ----------
+    aslist(input)
+    Returns the input as a list. If the input is a tuple, converts to a list. If
+    the input is a list, returns it unchanged. Otherwise, places the input within
+    a new list.
+    """
+    if isinstance(input, tuple):
+        input = list(input)
+    elif not isinstance(input, list):
+        input = [input,]
     return input
 
 
 def astuple(input: Any) -> Tuple:
-    'astuple  Place input in tuple if not already a tuple.'
-    if not isinstance(input, tuple):
+    """
+    astuple  Returns an input as a tuple
+    ----------
+    astuple(input)
+    Returns the input as a tuple. If the input is a list, converts to a tuple. If
+    the input is a tuple, returns it unchanged. Otherwise, places the input within
+    a new tuple.
+    """
+    if isinstance(input, list):
+        input = tuple(input)
+    elif not isinstance(input, tuple):
         input = (input,)
     return input
 
 
 def any_defined(*args: Any) -> bool:
-    "any_defined  True if any input is not None"
+    "any_defined  True if any input is not None. Otherwise False."
     for arg in args:
         if arg is not None:
             return True
