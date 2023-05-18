d21e0a2569e3f36d07c8c741fe00c281aa198c7b
jking <jking@usgs.gov>
2023-05-18 12:08:26 -0600

    organize utils

--- dfha/utils.py
+++ dfha/utils.py
@@ -15,6 +15,12 @@ Argument Parsing:
 Raster IO:
     load_raster     - Returns a pre-validated raster as a numpy array
     save_raster     - Saves a 2D numpy array raster to a GeoTIFF file
+
+NoData:
+    nodata_mask     - Returns the NoData mask for a raster array
+
+
+
     replace_nodata  - Replaces NoData values in a raster array with a specified value
 
 Raster Metadata:
@@ -26,7 +32,7 @@ from numpy import ndarray, integer, floating, bool_, isnan, full, any
 import rasterio
 from pathlib import Path
 from typing import List, Any, Tuple, Optional, Union
-from dfha.typing import RasterArray, ValidatedRaster, scalar, BooleanMask, nodata
+from dfha.typing import RasterArray, ValidatedRaster, scalar, RealArray, DataMask, nodata, BooleanMask
 from warnings import simplefilter, catch_warnings
 
 
@@ -40,6 +46,11 @@ def any_defined(*args: Any) -> bool:
         if arg is not None:
             return True
     return False
+
+
+#####
+# Sequences
+#####
 
 
 def aslist(input: Any) -> List:
@@ -72,6 +83,11 @@ def astuple(input: Any) -> Tuple:
     elif not isinstance(input, tuple):
         input = (input,)
     return input
+
+
+#####
+# Raster IO
+#####
 
 
 def load_raster(
@@ -143,6 +159,88 @@ def load_raster(
     if replace:
         raster, _ = replace_nodata(raster, nodata, nodata_to, copy=isarray)
     return raster
+
+
+def save_raster(
+    raster: RasterArray, path: Path, nodata: Optional[scalar] = None
+) -> None:
+    """
+    save_raster  Saves a numpy array (raster) to a GeoTIFF file
+    ----------
+    save_raster(raster, path)
+    Saves a 2D numpy array to the indicated GeoTIFF file. If the array is boolean,
+    saves as an int8 dtype. Note that this function does not currently support
+    geotransform options.
+
+    save_raster(raster, path, nodata)
+    Also specifies a nodata value for the raster. The nodata value is saved in
+    the GeoTIFF metadata (rather than as a nodata mask).
+    ----------
+    Inputs:
+        raster: A numpy 2D array. Should be an integer, floating, or boolean dtype
+        path: The path to the file in which to write the raster
+        nodata: A nodata value that should be saved in the GeoTIFF metadata.
+
+    Saves:
+        A GeoTIFF file matching the "path" input.
+    """
+
+    # Rasterio does not accept boolean dtype, so convert to (equivalent) int8
+    if raster.dtype == bool:
+        raster = raster.astype("int8")
+
+    # Temporarily disable the NotGeoreferencedWarning. This should eventually be
+    # replaced with functionality for crs and transform options.
+    with catch_warnings():
+        simplefilter("ignore", rasterio.errors.NotGeoreferencedWarning)
+
+        # Save the raster
+        with rasterio.open(
+            path,
+            "w",
+            driver="GTiff",
+            height=raster.shape[0],
+            width=raster.shape[1],
+            count=1,
+            dtype=raster.dtype,
+            nodata=nodata,
+        ) as file:
+            file.write(raster, 1)
+
+
+#####
+# NoData
+#####
+
+def data_mask(raster: RealArray, nodata: nodata) -> DataMask:
+    """
+    data_mask  Returns the valid data mask for a raster
+    ----------
+    data_mask(raster, nodata)
+    Given a NoData value, returns a mask that indicates the valid data elements
+    in the raster. True values indicate a valid data element, False values indicate
+    elements that are NoData. If the input NoData value is None, returns None,
+    as no masking is necessary.
+    ----------
+    Inputs:
+        raster: The raster whose valid data elements should be located
+        nodata: A NoData value for the raster
+
+    Outputs:
+        boolean numpy array | None: The valid data elements in the array, or 
+            None if there is not a NoData value.    
+    """
+    if nodata is None:
+        return None
+    elif isnan(nodata):
+        return ~isnan(raster)
+    else:
+        return raster != nodata
+    
+
+
+
+
 
 
 def raster_nodata(raster: ValidatedRaster, numpy_nodata: nodata):
@@ -253,50 +351,3 @@ def replace_nodata(
     elif return_mask:
         nodata = full(raster.shape, False)
     return raster, nodata
-
-
-def save_raster(
-    raster: RasterArray, path: Path, nodata: Optional[scalar] = None
-) -> None:
-    """
-    save_raster  Saves a numpy array (raster) to a GeoTIFF file
-    ----------
-    save_raster(raster, path)
-    Saves a 2D numpy array to the indicated GeoTIFF file. If the array is boolean,
-    saves as an int8 dtype. Note that this function does not currently support
-    geotransform options.
-
-    save_raster(raster, path, nodata)
-    Also specifies a nodata value for the raster. The nodata value is saved in
-    the GeoTIFF metadata (rather than as a nodata mask).
-    ----------
-    Inputs:
-        raster: A numpy 2D array. Should be an integer, floating, or boolean dtype
-        path: The path to the file in which to write the raster
-        nodata: A nodata value that should be saved in the GeoTIFF metadata.
-
-    Saves:
-        A GeoTIFF file matching the "path" input.
-    """
-
-    # Rasterio does not accept boolean dtype, so convert to (equivalent) int8
-    if raster.dtype == bool:
-        raster = raster.astype("int8")
-
-    # Temporarily disable the NotGeoreferencedWarning. This should eventually be
-    # replaced with functionality for crs and transform options.
-    with catch_warnings():
-        simplefilter("ignore", rasterio.errors.NotGeoreferencedWarning)
-
-        # Save the raster
-        with rasterio.open(
-            path,
-            "w",
-            driver="GTiff",
-            height=raster.shape[0],
-            width=raster.shape[1],
-            count=1,
-            dtype=raster.dtype,
-            nodata=nodata,
-        ) as file:
-            file.write(raster, 1)
