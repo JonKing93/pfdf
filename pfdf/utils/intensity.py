0f71f421bf5debf8d22add879ebc4e8fb0dea024
jking <jking@usgs.gov>
2024-05-28 12:51:47 -0600

    Reworded rainfall accumulation docs

--- pfdf/utils/intensity.py
+++ pfdf/utils/intensity.py
@@ -3,18 +3,17 @@ Functions for converting between rainfall accumulations and intensities
 ----------
 This module contains functions that convert between rainfall accumulations
 and intensities. In pfdf, the models.staley2017 (s17) module works strictly with 
-rainfall accumulations, specifically mm/duration, where duration is an interval
-in minutes. However, many users find it useful to work with rainfall intensities
-instead. Intensities are typically represented as mm/hour, whereas accumulations
-are mm for some duration of time (typically 15, 30, or 60 minutes). The functions 
+rainfall accumulations, defined as millimeters accumulated over a duration (typically
+15, 30, or 60 minutes). However, many users find it useful to instead work with 
+rainfall intensities, which are typically represented as mm/hour. The functions 
 in this module help convert between these two formats.
 
 These functions are specifically designed to support the s17 module. The
-"to_accumulation" converts from an input vector rainfall intensities to a vector
-of accumulations. By contrast, the "from_accumulation" function is intended to
-convert the output of the s17.likelihood fuction to intensities. This function
+"to_accumulation" function converts from an input vector rainfall intensities to 
+a vector of accumulations. By contrast, the "from_accumulation" function is intended 
+to convert the output of the s17.likelihood fuction to intensities. This function
 broadcasts a vector of durations along the second dimension of an accumulation
-array in order to convert the array to intensities.
+array in order to convert the array values to intensities.
 ----------
 Functions:
     to_accumulation     - Converts rainfall intensities to accumulations
@@ -35,16 +34,16 @@ def to_accumulation(I: vector, durations: vector) -> VectorArray:
     ----------
     to_accumulation(I, durations)
     Converts the input rainfall intensities (in mm/hour) to rainfall accumulations
-    (mm/duration). The input intensities should be a vector in mm/hour. The input
-    durations be in minutes. The durations may either be scalar, or a vector with
-    one element per intensity.
+    (mm accumulated over a duration). The input intensities should be a vector in 
+    mm/hour. The input durations should be in minutes. The durations may either 
+    be scalar, or a vector with one element per intensity.
     ----------
     Inputs:
         I: Rainfall intensities (mm/hour)
         durations: Number of minutes per duration
 
     Outputs:
-        numpy array: The converted rainfall accumulations (mm/duration)
+        numpy array: The converted rainfall accumulations
     """
 
     # Intensities should be a vector
@@ -66,14 +65,15 @@ def from_accumulation(R: RealArray, durations: vector) -> RealArray:
     Converts rainfall accumulations to intensities
     ----------
     from_accumulation(R, durations)
-    Converts the input rainfall accumulations from mm/duration to rainfall
-    intensities in mm/hour. R should be an array of values in mm/durations. The
-    array is assumed to originate from the s17.accumulation function, so durations
-    are broadcast across the second dimension. The input durations should be in
-    minutes and may either be scalar or a vector with one element per column in R.
+    Converts the input rainfall accumulations from mm over a duration to rainfall
+    intensities in mm/hour. R should be an array of values representing millimeters
+    of accumulation over one or more durations. The array is assumed to originate 
+    from the s17.accumulation function, so durations are broadcast across the 
+    second dimension. The input durations should be in minutes and may either be 
+    scalar or a vector with one element per column in R.
     ----------
     Inputs:
-        R: An array of rainfall accumulations in mm/duration
+        R: An array of rainfall accumulations in millimeters over durations
         durations: Rainfall durations in minutes. Either scalar, or a vector
             with one element per column of R
 
